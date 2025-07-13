51ceab83cab96352ffb4ba0c719d9ffb794024c2
Akash Patil <akashivu002@gmail.com>
2025-07-13 12:38:02 +0530

    feat: add JobController with post and get endpoints

---
+++ src/main/java/com/jobportal/job_portal_backend/controller/JobController.java
@@ -0,0 +1,28 @@
+package com.jobportal.job_portal_backend.Controller;
+
+import com.jobportal.job_portal_backend.Dto.JobDto;
+import com.jobportal.job_portal_backend.Service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/jobs")
+public class JobController {
+
+    @Autowired
+    private JobService jobService;
+
+   
+    @PostMapping
+    public JobDto postJob(@RequestBody JobDto jobDto, @RequestParam Long userId) {
+        return jobService.createJob(jobDto, userId);
+    }
+
+
+    @GetMapping
+    public List<JobDto> getAllJobs() {
+        return jobService.getAllJobs();
+    }
+}

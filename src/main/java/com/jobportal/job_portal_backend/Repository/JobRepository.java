aac195d6f7a146c1c4a8efc5814d7930aaf4b450
Akash Patil <akashivu002@gmail.com>
2025-07-14 10:31:28 +0530

    feat: add job search filtering by title and location

--- src/main/java/com/jobportal/job_portal_backend/Repository/JobRepository.java
+++ src/main/java/com/jobportal/job_portal_backend/Repository/JobRepository.java
@@ -2,7 +2,15 @@ package com.jobportal.job_portal_backend.Repository;
 
 import com.jobportal.job_portal_backend.Entity.Job;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface JobRepository extends JpaRepository<Job, Long> {
+    @Query("SELECT j FROM Job j WHERE " +
+            "(:title IS NULL OR LOWER(j.title) LIKE LOWER(CONCAT('%', :title, '%'))) AND " +
+            "(:location IS NULL OR LOWER(j.location) LIKE LOWER(CONCAT('%', :location, '%')))")
+    List<Job> searchJobs(@Param("title") String title, @Param("location") String location);
 
 }

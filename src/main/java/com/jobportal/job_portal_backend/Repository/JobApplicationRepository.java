b9cc7bed432327d35e25ecc537244d8e994a1734
Akash Patil <akashivu002@gmail.com>
2025-07-20 11:53:15 +0530

    Create JobApplicationRepository with custom finder methods

---
+++ src/main/java/com/jobportal/job_portal_backend/Repository/JobApplicationRepository.java
@@ -0,0 +1,13 @@
+package com.jobportal.job_portal_backend.Repository;
+
+import com.jobportal.job_portal_backend.Entity.JobApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface JobApplicationRepository extends JpaRepository<JobApplication, Long> {
+    List<JobApplication> findByUserId(Long userId);
+    boolean existsByUserIdAndJobId(Long userId, Long jobId);
+}

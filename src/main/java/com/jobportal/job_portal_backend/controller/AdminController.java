737a731203a10ac8811faed5056c3030eb40c270
Akash Patil <akashivu002@gmail.com>
2025-07-25 11:24:57 +0530

    Replaced direct UserRepository access in AdminController with UserService

--- src/main/java/com/jobportal/job_portal_backend/controller/AdminController.java
+++ src/main/java/com/jobportal/job_portal_backend/controller/AdminController.java
@@ -1,15 +1,14 @@
 package com.jobportal.job_portal_backend.Controller;
 
+import com.jobportal.job_portal_backend.Dto.UserDto;
 import com.jobportal.job_portal_backend.Entity.User;
 import com.jobportal.job_portal_backend.Repository.UserRepository;
+import com.jobportal.job_portal_backend.Service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,15 +17,17 @@ import java.util.List;
 @RequestMapping("/api/admin")
 @PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
-    @Autowired
-    private UserRepository userRepository;
+
+
+    private UserService userService;
     @GetMapping("/users")
-    public ResponseEntity<List<User>> getAllUser(){
-        return ResponseEntity.ok(userRepository.findAll());
+    public ResponseEntity<List<UserDto>> getAllUser(){
+        return ResponseEntity.ok(userService.getAllUsers());
     }
-  public ResponseEntity<String> deleteUser(@PathVariable Long id){
-        userRepository.deleteById(id);
-        return ResponseEntity.ok("user deleted succesfully");
-  }
+    @DeleteMapping("/user/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("User deleted successfully");
+    }
 
 }

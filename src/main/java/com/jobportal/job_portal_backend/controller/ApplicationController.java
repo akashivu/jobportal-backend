452ba98c7e6344ff57f5d07d8a81bfc9045a3839
Akash Patil <akashivu002@gmail.com>
2025-08-02 12:07:21 +0530

    Add API to download user resume by filename

--- src/main/java/com/jobportal/job_portal_backend/controller/ApplicationController.java
+++ src/main/java/com/jobportal/job_portal_backend/controller/ApplicationController.java
@@ -2,8 +2,12 @@ package com.jobportal.job_portal_backend.Controller;
 
 import com.jobportal.job_portal_backend.Entity.JobApplication;
 import com.jobportal.job_portal_backend.Service.ApplicationService;
+import com.jobportal.job_portal_backend.Service.FileStorageService;
 import com.jobportal.job_portal_backend.Service.JobService;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +21,8 @@ public class ApplicationController {
       private JobService jobService;
      @Autowired
       private ApplicationService applicationService;
+     @Autowired
+     private FileStorageService fileStorageService;
     @PostMapping("/{jobId}")
     public ResponseEntity<String> applyToJob(@PathVariable Long jobId, @RequestPart("resume") MultipartFile resume, Authentication authentication){
         String email = authentication.getName();
@@ -33,4 +39,13 @@ public class ApplicationController {
         List<JobApplication> applications = applicationService.getApplicationByJob(jobId);
         return ResponseEntity.ok(applications);
     }
+    @GetMapping("/resume/{fileName}")
+    public ResponseEntity<Resource> downloadResume(@PathVariable String fileName) {
+        Resource resource = fileStorageService.loadFile(fileName);
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"")
+                .body(resource);
+    }
+
 }
